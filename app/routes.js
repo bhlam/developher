831ada2e038921bed99fe23e9044823d2d484650
Brianna Lam <bhlam@ucsd.edu>
2015-08-29 07:56:12 -0700

    more routes

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,6 @@
-var user = require('./models/user');
+var User = require('./models/user');
 
-module.exports = module.exports = function(app, passport){
+module.exports = function(app, passport){
 	// app.get('/', function(req, res){
 	// 	res.render('index.ejs');
 	// });
@@ -29,6 +29,28 @@ module.exports = module.exports = function(app, passport){
 	app.get('/create', isLoggedIn, function(req, res){
 		res.render('createprofile.ejs', { user: req.user });
 	});
+
+	app.post('/create', function(req, res){
+		console.log('is there a user? ' + req.user);
+		User.findOne({'general.email':req.user.email, 'general.password':req.user.password}, function(err, user){
+		 	if(err){
+		 		throw err;
+		 	}
+		 	if(user){
+		 		console.log('user exists');
+		 		console.log('the age is ' + req.body.age);
+		 		user.personal.age = req.body.age;
+		 		user.personal.current_city = req.body.city;
+		 		user.save(function(err){
+		 			if(err)
+						throw err;
+					return done(null, newUser);
+				});
+		 	}
+		 });
+		res.redirect('/profile');
+	});
+
 
 	app.get('/profile', isLoggedIn, function(req, res){
 		res.render('profile.ejs', { user: req.user });
